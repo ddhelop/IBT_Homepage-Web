38a0a32e4bcc3ca906d2e902bd19bec41c80155b
HIHJH <2170095@ewhain.net>
2024-02-02 20:12:21 +0900

    style: 기본 세팅 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { cls, notoSansKr, roboto } from "@/lib/fonts";
-import RecoilContextProvider from "@/context/recoil-context";
-import ActiveSectionContextProvider from "@/context/active-section-context";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { cls, notoSansKr, roboto } from '@/lib/fonts'
+import RecoilContextProvider from '@/context/recoil-context'
+import ActiveSectionContextProvider from '@/context/active-section-context'
 
 export const metadata: Metadata = {
-  title: "IBT",
+  title: 'IBT',
   description:
-    "IBT는 혁신적인 배터리 기술을 통해 지속 가능한 에너지 솔루션을 선도하며, 새로운 에너지 라이프스타일을 창조하고 있습니다.",
-};
+    'IBT는 혁신적인 배터리 기술을 통해 지속 가능한 에너지 솔루션을 선도하며, 새로운 에너지 라이프스타일을 창조하고 있습니다.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${cls(
-          notoSansKr.className,
-          roboto.variable
-        )} transition  text-gray-950 relative pt-28`}
-      >
+      <body className={`${cls(notoSansKr.className, roboto.variable)} transition  text-gray-950 relative pt-28`}>
         <ActiveSectionContextProvider>
           <RecoilContextProvider>{children}</RecoilContextProvider>
         </ActiveSectionContextProvider>
       </body>
     </html>
-  );
+  )
 }
