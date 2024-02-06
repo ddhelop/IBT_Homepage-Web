f0a68f6371011307899db0d10eb9a13b25c8b9cf
HIHJH <2170095@ewhain.net>
2024-02-07 00:07:56 +0900

    feat: 기타 페이지 연결

--- src/components/Floating.tsx
+++ src/components/Floating.tsx
@@ -26,7 +26,9 @@ const FloatingButton = () => {
           <Link href="/battery/batteryDetail/energy-save">
             <div className="bg-slate-100 px-2 py-2 hover:bg-primary-green hover:text-white">에너지저장</div>
           </Link>
-          <div className="bg-slate-400 px-2 py-2 hover:bg-primary-green hover:text-white">기타</div>
+          <Link href="/battery/batteryDetail/others">
+            <div className="bg-slate-400 px-2 py-2 hover:bg-primary-green hover:text-white">기타</div>
+          </Link>
           <Link href="/about-corp">
             <div className="bg-slate-300 px-2 py-2 hover:bg-primary-green hover:text-white">카탈로그</div>
           </Link>
