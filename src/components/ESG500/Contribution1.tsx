63e81b74fdfe780d2d3d3ba81378c8b24ca8e766
김동혁 <clfgnm9@naver.com>
2024-02-08 17:45:10 +0900

    feat: :lipstick: ESG 태블릿 반응형 추가 및 UI 수정

--- src/components/ESG500/Contribution1.tsx
+++ src/components/ESG500/Contribution1.tsx
@@ -14,13 +14,13 @@ export default function Contribution1() {
           className="mb-28 mt-28"
         />
         {/* 첫 단락 */}
-        <div className="w-full flex flex-col md:flex-row p-3">
-          <div className="md:w-1/3 flex">
-            <div className="md:w-1/2 h-20 border-t-2 text-xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-center">
+        <div className="w-full flex flex-col lg:flex-row p-3">
+          <div className="lg:w-1/4 flex">
+            <div className=" h-20 border-t-2 text-xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-center">
               재단소개 및 사업내용
             </div>
           </div>
-          <p className="md:w-2/3 text-xl leading-9 tracking-wider text-[#7b7b7b]">
+          <p className="lg:w-3/4 text-xl leading-9 tracking-wider text-[#7b7b7b]">
             안녕하세요. 지우장학회 이사장 장민기입니다.
             <br />
             지우장학회는 2000년 광주, 전남 지역의 골프 스포츠 인재 육성을 위해 설립되었습니다. 설립 이후 대회 후원과
@@ -35,23 +35,23 @@ export default function Contribution1() {
         </div>
 
         {/* 두번째 단락 */}
-        <div className="w-full flex flex-col md:flex-row p-3 mt-24">
-          <div className="md:w-1/3 flex ">
-            <div className="md:w-1/2 h-[80px] border-t-2 text-[20px] tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
-              <p className="md:pl-6">투명 경영</p>
+        <div className="w-full flex flex-col lg:flex-row p-3 mt-24">
+          <div className="lg:w-1/4 flex ">
+            <div className=" h-[80px] border-t-2 text-2xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
+              <p className="lg:pl-6">투명 경영</p>
             </div>
           </div>
-          <div className="md:w-2/3 flex flex-col">
-            <p className="text-[20px] leading-9 tracking-wider text-[#7b7b7b]">
+          <div className="lg:w-3/4 flex flex-col">
+            <p className="text-xl leading-9 tracking-wider text-[#7b7b7b]">
               지우장학회는 운영과 관련한 모든 법률과 규정을 준수 하고 있으며, 회계 전문가를 통해 철저히 내부 관리를
               실시하고 있습니다.
               <br />
               기부 빛 후원은 &apos;기부금품의 모집 및 사용에 관한 법률&apos;등 관련 법률을 따르고 있습니다.
             </p>
-            <h3 className="text-[35px] mt-28 font-medium text-[#787878]">기부금 모금 및 사용 현황</h3>
+            <h3 className="text-4xl mt-28 font-medium text-[#787878]">기부금 모금 및 사용 현황</h3>
             {/* table */}
             <div className="overflow-x-auto mt-8 text-[#7b7b7b]">
-              <p className="text-right text-[20px]">(단위:만원)</p>
+              <p className="text-right text-xl">(단위:만원)</p>
               <table className="min-w-full">
                 <thead className="bg-green-600 text-white tracking-[-0.1rem] h-[50px]">
                   <tr>
@@ -96,12 +96,10 @@ export default function Contribution1() {
               </table>
             </div>
             {/* table end*/}
-            <p className="text-[18px] leading-9 tracking-wider text-[#7b7b7b]">
-              기부금은 장학 목적 사업에만 사용 됩니다.
-            </p>
+            <p className="text-lg leading-9 tracking-wider text-[#7b7b7b]">기부금은 장학 목적 사업에만 사용 됩니다.</p>
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_22.pdf"
-              className="text-[18px] hover:text-[#000000] mt-12 tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-lg hover:text-[#000000] mt-12 tracking-wider text-[#7b7b7b] cursor-pointer"
               target="_blank"
             >
               지우장학회 22년 기부금모금액 및 활용실적명세.pdf
@@ -109,29 +107,29 @@ export default function Contribution1() {
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_21.pdf"
               target="_blank"
-              className="text-[18px] hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-lg hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
             >
               지우장학회 21년 기부금모금액 및 활용실적명세.pdf
             </a>
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_20.pdf"
               target="_blank"
-              className="text-[18px] mt-2 hover:text-[#000000] tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-lg mt-2 hover:text-[#000000] tracking-wider text-[#7b7b7b] cursor-pointer"
             >
               지우장학회 20년 기부금모금액 및 활용실적명세.pdf
             </a>
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_19.pdf"
               target="_blank"
-              className="text-[18px] hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-lg hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
             >
               지우장학회 19년 기부금모금액 및 활용실적명세.pdf
             </a>
 
-            <p className="text-[20px] mt-16 leading-9 tracking-wider text-[#7b7b7b]">
+            <p className="text-xl mt-16 leading-9 tracking-wider text-[#7b7b7b]">
               기부 및 후원은 전액 장학 목적 사업에만 사용되며 문의사항은 아래로 부탁드립니다.
             </p>
-            <p className="text-[20px] mt-12 leading-9 tracking-wider text-[#7b7b7b]">
+            <p className="text-xl mt-12 leading-9 tracking-wider text-[#7b7b7b]">
               광주 북구 첨단벤처소로28번길 2(월출동) 062-971-7983
               <br />
               서울 광진구 광나루토 56길 85, 1902호(구의동) 02-3424-1988 재단법인 중앙장형태기념재단 재단법인 지우장학회
--- src/components/ESG500/ESG500Component.tsx
+++ src/components/ESG500/ESG500Component.tsx
@@ -5,15 +5,15 @@ import SocialContributionComponent from './SocialContributionComponent'
 export default function ESG500Component() {
   return (
     <>
-      <div className="w-full md:p-[160px] flex flex-col min-h-screen bg-no-repeat bg-cover bg-white ">
-        <div>
+      <div className="w-full p-5 md:p-20 flex flex-col items-center lg:items-start min-h-screen bg-no-repeat bg-cover bg-white ">
+        <div className="flex flex-col max-w-[27rem] lg:max-w-none">
           <Image
             src="/ESG500/eco_friendly.svg" // 이미지 경로
             alt="eco_friendly"
             width={487} // 너비
             height={194} // 높이
             // layout="fixed" // 레이아웃 옵션
-            className="mb-32 md:ml-[108px]"
+            className="mb-32 lg:ml-[108px]"
           />
           <Image
             src="/ESG500/Eclipse1.png" // 이미지 경로
@@ -30,25 +30,25 @@ export default function ESG500Component() {
 
         {/* 세번째 단락 */}
 
-        <div className="w-full mt-36 md:mt-72">
+        <div className="w-full mt-36 lg:mt-72 flex flex-col items-center lg:items-start">
           <Image
             src="/ESG500/Reliable_Governance.svg" // 이미지 경로
             alt="Reliable_Governance"
             width={715} // 너비
-            height={122} // 높이
+            height={194} // 높이
             // layout="responsive" // 레이아웃 옵션
-            className="mb-32 md:ml-[108px] max-w-screen-md"
+            className="mb-24 lg:ml-[108px] lg:max-w-screen-md"
           />
-          <h1 className="text-4xl font-medium md:mr-[48%] text-center">윤리경영 보고서</h1>
+          <h1 className="text-5xl font-medium lg:ml-56 lg:my-12">윤리경영 보고서</h1>
           <div className="w-full flex flex-col justify-end items-center">
-            <div className="md:w-2/3 flex flex-col md:flex-row p-3 mt-28">
-              <div className="md:w-1/3 flex w-full">
-                <div className="md:w-1/2 h-[40px] text-[20px] tracking-wider text-[#9a9a9a] flex items-center justify-start">
-                  <p className="md:pl-6">재단 소개 및 사업내용</p>
+            <div className="lg:w-3/4 flex flex-col lg:flex-row p-3 mt-28">
+              <div className="lg:w-1/4 flex w-full">
+                <div className=" h-[40px] text-xl tracking-wider text-[#9a9a9a] flex items-center justify-start">
+                  <p className="">재단 소개 및 사업내용</p>
                 </div>
               </div>
-              <div className="md:w-2/3 flex flex-col">
-                <p className="text-[20px] leading-9 tracking-wider text-[#7b7b7b]">
+              <div className="lg:w-3/4 flex flex-col">
+                <p className="text-xl leading-9 tracking-wider text-[#7b7b7b]">
                   지우장학회는 2000년 광주, 전남 지역의 골프 스포츠 인재 육성을 위해 설립되었습니다. 설립 이후 대회
                   후원과 골프 유망주 지원 등을 꾸준히 이어오고 있습니다.
                   <br />
@@ -62,28 +62,28 @@ export default function ESG500Component() {
             </div>
           </div>
 
-          <div className="w-full md:flex md:flex-col justify-end items-center">
-            <div className="md:w-2/3 flex flex-col md:flex-row p-3 mt-28">
-              <div className="md:w-1/3 flex">
-                <div className="md:w-1/2 h-[80px] border-t-2 text-[20px] tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
+          <div className="w-full lg:flex md:flex-col justify-end items-center">
+            <div className="lg:w-3/4 flex flex-col lg:flex-row p-3 mt-28">
+              <div className="lg:w-1/4 flex">
+                <div className=" h-[80px] border-t-2 text-xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
                   투명 경영
                 </div>
               </div>
 
-              <div className="md:w-2/3 flex flex-col">
-                <p className="text-[20px] leading-9 tracking-wider text-[#7b7b7b]">
+              <div className="lg:w-3/4 flex flex-col">
+                <p className="text-xl leading-9 tracking-wider text-[#7b7b7b]">
                   지우장학회는 운영과 관련한 모든 법률과 규정을 준수하고 있으며, 회계 전문가를 통해 철저한 내부 관리를
                   실시하고 있습니다.
                   <br />
                   기부 및 후원은 &apos;기금금품의 모집 및 사용에 관한 법률&apos;등 관련 법률을 따르고 있습니다.
                 </p>
 
-                <h3 className="text-[35px] mt-28 font-medium text-[#787878]">기부금 모금 및 사용 현황</h3>
+                <h3 className="text-4xl mt-28 font-medium text-[#787878]">기부금 모금 및 사용 현황</h3>
                 {/* table */}
                 <div className="overflow-x-auto mt-8 text-[#7b7b7b]">
-                  <p className="text-right text-[20px]">(단위:만원)</p>
-                  <table className="min-w-full">
-                    <thead className="bg-green-600 text-white tracking-[-0.1rem] h-[50px]">
+                  <p className="text-right text-xl">(단위:만원)</p>
+                  <table className="max-w-full">
+                    <thead className="bg-green-600 text-white tracking-[-0.1rem] h-12">
                       <tr>
                         <th className="px-4 py-2"></th>
                         <th className="px-4 py-2">2019년</th>
@@ -127,12 +127,12 @@ export default function ESG500Component() {
                 </div>
                 {/* table end*/}
 
-                <p className="text-[18px] leading-9 tracking-wider text-[#7b7b7b]">
+                <p className="text-lg leading-9 tracking-wider text-[#7b7b7b]">
                   기부금은 장학 목적 사업에만 사용 됩니다.
                 </p>
                 <a
                   href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_22.pdf"
-                  className="text-[18px] hover:text-[#000000] mt-12 tracking-wider text-[#7b7b7b] cursor-pointer"
+                  className="text-lg hover:text-[#000000] mt-12 tracking-wider text-[#7b7b7b] cursor-pointer"
                   target="_blank"
                 >
                   지우장학회 22년 기부금모금액 및 활용실적명세.pdf
@@ -140,21 +140,21 @@ export default function ESG500Component() {
                 <a
                   href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_21.pdf"
                   target="_blank"
-                  className="text-[18px] hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
+                  className="text-lg hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
                 >
                   지우장학회 21년 기부금모금액 및 활용실적명세.pdf
                 </a>
                 <a
                   href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_20.pdf"
                   target="_blank"
-                  className="text-[18px] mt-2 hover:text-[#000000] tracking-wider text-[#7b7b7b] cursor-pointer"
+                  className="text-lg mt-2 hover:text-[#000000] tracking-wider text-[#7b7b7b] cursor-pointer"
                 >
                   지우장학회 20년 기부금모금액 및 활용실적명세.pdf
                 </a>
                 <a
                   href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_19.pdf"
                   target="_blank"
-                  className="text-[18px] hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
+                  className="text-lg hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
                 >
                   지우장학회 19년 기부금모금액 및 활용실적명세.pdf
                 </a>
@@ -163,17 +163,17 @@ export default function ESG500Component() {
           </div>
 
           <div className="w-full flex flex-col justify-end items-center">
-            <div className="md:w-2/3 flex flex-col md:flex-row p-3 mt-28">
-              <div className="md:w-1/3 flex">
-                <div className="md:w-1/2 h-[80px] border-t-2 text-[20px] tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
+            <div className="lg:w-3/4 flex flex-col lg:flex-row p-3 mt-28">
+              <div className="lg:w-1/4 flex">
+                <div className="h-20 border-t-2 text-xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
                   기부 및 후원 문의
                 </div>
               </div>
-              <div className="md:w-2/3 flex flex-col">
-                <p className="text-[20px] leading-9 tracking-wider text-[#7b7b7b]">
+              <div className="lg:w-3/4 flex flex-col">
+                <p className="text-xl leading-9 tracking-wider text-[#7b7b7b]">
                   기부 및 후원은 전액 장학 목적 사업에만 사용되며 문의사항은 아래로 부탁드립니다.
                 </p>
-                <p className="text-[20px] leading-9 tracking-wider text-[#7b7b7b] mt-12">
+                <p className="text-xl leading-9 tracking-wider text-[#7b7b7b] mt-12">
                   광주 북구 첨단벤처소로28번길 2(월출동) 062-971-7983
                   <br />
                   서울 광진구 광나루로 56길 85, 1902호(구의동) 02-3424-1988 재단법인중양장형태기념재단 재단법인 지우장
