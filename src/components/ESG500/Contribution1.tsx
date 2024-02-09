110a49d627c16fd676390f3cf8f62f4073750cdb
김동혁 <clfgnm9@naver.com>
2024-02-09 15:53:59 +0900

    refactor: :lipstick: ESG500 페이지 전체적인 크기 재조정 및 보완

--- src/components/ESG500/Contribution1.tsx
+++ src/components/ESG500/Contribution1.tsx
@@ -16,11 +16,11 @@ export default function Contribution1() {
         {/* 첫 단락 */}
         <div className="w-full flex flex-col lg:flex-row p-3">
           <div className="lg:w-1/4 flex">
-            <div className=" h-20 border-t-2 text-xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-center">
+            <div className=" h-20 border-t-2 text-base tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-center">
               재단소개 및 사업내용
             </div>
           </div>
-          <p className="lg:w-3/4 text-xl leading-9 tracking-wider text-[#7b7b7b]">
+          <p className="lg:w-3/4 text-base leading-9 tracking-wider text-[#7b7b7b]">
             안녕하세요. 지우장학회 이사장 장민기입니다.
             <br />
             지우장학회는 2000년 광주, 전남 지역의 골프 스포츠 인재 육성을 위해 설립되었습니다. 설립 이후 대회 후원과
@@ -37,23 +37,23 @@ export default function Contribution1() {
         {/* 두번째 단락 */}
         <div className="w-full flex flex-col lg:flex-row p-3 mt-24">
           <div className="lg:w-1/4 flex ">
-            <div className=" h-20 border-t-2 text-xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
+            <div className=" h-20 border-t-2 text-base tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-start">
               <p className="lg:pr-28">투명 경영</p>
             </div>
           </div>
           <div className="lg:w-3/4 flex flex-col">
-            <p className="text-xl leading-9 tracking-wider text-[#7b7b7b]">
+            <p className="text-base leading-9 tracking-wider text-[#7b7b7b]">
               지우장학회는 운영과 관련한 모든 법률과 규정을 준수 하고 있으며, 회계 전문가를 통해 철저히 내부 관리를
               실시하고 있습니다.
               <br />
               기부 빛 후원은 &apos;기부금품의 모집 및 사용에 관한 법률&apos;등 관련 법률을 따르고 있습니다.
             </p>
-            <h3 className="text-4xl mt-28 font-medium text-[#787878]">기부금 모금 및 사용 현황</h3>
+            <h3 className="text-3xl mt-28 font-medium text-[#787878]">기부금 모금 및 사용 현황</h3>
             {/* table */}
             <div className="overflow-x-auto mt-8 text-[#7b7b7b]">
-              <p className="text-right text-xl">(단위:만원)</p>
+              <p className="text-right text-base">(단위:만원)</p>
               <table className="min-w-full">
-                <thead className="bg-green-600 text-white tracking-[-0.1rem] h-12">
+                <thead className="bg-green-600 text-white tracking-[-0.1rem] h-12 text-sm">
                   <tr>
                     <th className="px-4 py-2"></th>
                     <th className="px-4 py-2">2019년</th>
@@ -67,7 +67,7 @@ export default function Contribution1() {
                     <th className="px-4 py-2">2027년</th>
                   </tr>
                 </thead>
-                <tbody>
+                <tbody className="text-sm">
                   <tr className="bg-[#f0f0f0]">
                     <td className="border px-4 py-2 text-center">기부금 모금</td>
                     <td className="border px-4 py-2 text-center">500</td>
@@ -96,10 +96,12 @@ export default function Contribution1() {
               </table>
             </div>
             {/* table end*/}
-            <p className="text-lg leading-9 tracking-wider text-[#7b7b7b]">기부금은 장학 목적 사업에만 사용 됩니다.</p>
+            <p className="text-base leading-9 tracking-wider text-[#7b7b7b]">
+              기부금은 장학 목적 사업에만 사용 됩니다.
+            </p>
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_22.pdf"
-              className="text-lg hover:text-[#000000] mt-12 tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-base hover:text-[#000000] mt-12 tracking-wider text-[#7b7b7b] cursor-pointer"
               target="_blank"
             >
               지우장학회 22년 기부금모금액 및 활용실적명세.pdf
@@ -107,21 +109,21 @@ export default function Contribution1() {
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_21.pdf"
               target="_blank"
-              className="text-lg hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-base hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
             >
               지우장학회 21년 기부금모금액 및 활용실적명세.pdf
             </a>
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_20.pdf"
               target="_blank"
-              className="text-lg mt-2 hover:text-[#000000] tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-base mt-2 hover:text-[#000000] tracking-wider text-[#7b7b7b] cursor-pointer"
             >
               지우장학회 20년 기부금모금액 및 활용실적명세.pdf
             </a>
             <a
               href="http://rocketibt.cafe24.com/bizdemo101303/img/images/ibt_pdf_19.pdf"
               target="_blank"
-              className="text-lg hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
+              className="text-base hover:text-[#000000] mt-2 tracking-wider text-[#7b7b7b] cursor-pointer"
             >
               지우장학회 19년 기부금모금액 및 활용실적명세.pdf
             </a>
@@ -130,16 +132,16 @@ export default function Contribution1() {
 
         <div className="w-full flex flex-col lg:flex-row mt-20 p-3">
           <div className="lg:w-1/4 flex ">
-            <div className="h-20 border-t-2 text-xl tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-center">
+            <div className="h-20 border-t-2 text-base tracking-wider text-[#9a9a9a] border-t-[#4be7ff] flex items-center justify-center">
               <p className="lg:pr-6">기부 및 후원 문의</p>
             </div>
           </div>
           <div className="lg:w-3/4">
-            <p className="text-xl leading-9 tracking-wider text-[#7b7b7b]">
+            <p className="text-base leading-9 tracking-wider text-[#7b7b7b]">
               기부 및 후원은 전액 장학 목적 사업에만 사용되며 문의사항은 아래로 부탁드립니다.
             </p>
 
-            <p className="text-xl mt-12 leading-9 tracking-wider text-[#7b7b7b]">
+            <p className="text-base mt-12 leading-9 tracking-wider text-[#7b7b7b]">
               광주 북구 첨단벤처소로28번길 2(월출동) 062-971-7983
               <br />
               서울 광진구 광나루토 56길 85, 1902호(구의동) 02-3424-1988 재단법인 중앙장형태기념재단 재단법인 지우장학회
