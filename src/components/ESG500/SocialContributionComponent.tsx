a4ff95f4095bf82814af860e9d543f494778a2b8
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-02-24 02:26:46 +0900

    refactor: :lipstick: adjust ESG_500 page style (font, layout)

--- src/components/ESG500/SocialContributionComponent.tsx
+++ src/components/ESG500/SocialContributionComponent.tsx
@@ -35,7 +35,7 @@ export default function SocialContributionComponent({ data }: SocialProps) {
               <Image
                 src="/ESG500/Social_Contribution.svg" // 이미지 경로
                 alt="Social_Contribution"
-                width={646} // 너비
+                width={430} // 너비
                 height={194} // 높이
                 // layout="responsive" // 레이아웃 옵션
                 className="lg:my-24"
@@ -44,16 +44,16 @@ export default function SocialContributionComponent({ data }: SocialProps) {
           </div>
 
           {/* Tabs */}
-          <LeftMotionComponent component={motion.div} className="mt-24 w-full flex items-center justify-center">
+          <LeftMotionComponent component={motion.div} className="mt-20 lg:mt-0 w-full flex items-center justify-center">
             <button
-              className={`w-2/5 lg:w-2/5 h-12 tab-button1 ${isActive('tab1') ? 'active-tab1' : ''}`}
+              className={`w-1/3 h-11 tab-button1 ${isActive('tab1') ? 'active-tab1' : ''}`}
               value="tab1"
               onClick={onClickButton}
             >
               (재) 지우장학회
             </button>
             <button
-              className={`w-2/5 lg:w-2/5 h-12  tab-button2 ${isActive('tab2') ? 'active-tab2' : ''}`}
+              className={`w-1/3 h-11  tab-button2 ${isActive('tab2') ? 'active-tab2' : ''}`}
               onClick={() => setActiveTab('tab2')}
             >
               중앙장형태 장학재단
