9dba8efdfc14c118668238e1652c5e1bca66b01c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-02-23 18:57:23 +0900

    feat: :zap: mobile intro 협력사  애니메이션 적용

--- src/components/intro/IntroContentBlock.client.tsx
+++ src/components/intro/IntroContentBlock.client.tsx
@@ -1,26 +1,9 @@
 'use client'
-import { useEffect, useRef, useState } from 'react'
-import { motion, useAnimation } from 'framer-motion'
+
+import { motion } from 'framer-motion'
 import { IContentBlockProps } from './IntroComponent.types'
-import { useInView } from 'react-intersection-observer'
-import { useScrollAnimation } from '../commons/UseScrollAnimation'
 
 export default function ContentBlock(props: IContentBlockProps) {
-  const { ref, control } = useScrollAnimation()
-
-  // text framer motion
-  const boxVariant = {
-    visible: {
-      opacity: 1,
-      scale: 1.1,
-      transition: {
-        duration: 0.7,
-        ease: [0, 0.4, 0.8, 1.2],
-      },
-    },
-    hidden: { opacity: 0, scale: 0 },
-  }
-
   return (
     <div
       style={{ backgroundImage: `url(${props.background})` }}
@@ -29,19 +12,23 @@ export default function ContentBlock(props: IContentBlockProps) {
       {/* contentRef를 적용할 새로운 div 추가 */}
       <div>
         <motion.h1
-          ref={ref}
-          variants={boxVariant}
-          initial="hidden"
-          animate={control}
+          initial={{ opacity: 0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1.1 }}
+          transition={{
+            duration: 0.7,
+            ease: [0, 0.4, 0.8, 1.2],
+          }}
           className={`text-5xl font-bold mb-4 text-white `}
         >
           {props.title}
         </motion.h1>
         <motion.p
-          ref={ref}
-          variants={boxVariant}
-          initial="hidden"
-          animate={control}
+          initial={{ opacity: 0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1.1 }}
+          transition={{
+            duration: 0.7,
+            ease: [0, 0.4, 0.8, 1.2],
+          }}
           className="leading-relaxed whitespace-pre-line text-xl px-8 font-bold md:font-light mt-20 text-white"
         >
           {props.text}
