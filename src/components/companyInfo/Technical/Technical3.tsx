2b82f1b5df3e8b3877fbcd24f1c274263d2ae38c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-02-25 00:51:45 +0900

    style: :lipstick: adjust companyInfo overall size(font, box)

--- src/components/companyInfo/Technical/Technical3.tsx
+++ src/components/companyInfo/Technical/Technical3.tsx
@@ -9,19 +9,19 @@ export default function Technical3() {
           <table className="w-full">
             <thead className="l bg-green-600">
               <tr className="">
-                <th scope="col" className="w-[100px] text-base font-semibold text-white py-4">
+                <th scope="col" className="w-[100px] text-sm font-semibold text-white py-3">
                   개발완료
                 </th>
-                <th scope="col" className="w-[310px] text-base font-semibold text-white">
+                <th scope="col" className="w-[310px] text-sm font-semibold text-white">
                   개발 제품명
                 </th>
-                <th scope="col" className="w-[400px] text-base font-semibold text-white">
+                <th scope="col" className="w-[400px] text-sm font-semibold text-white">
                   개발내용
                 </th>
-                <th scope="col" className="w-[240px] text-base font-semibold text-white">
+                <th scope="col" className="w-[240px] text-sm font-semibold text-white">
                   납품처
                 </th>
-                <th scope="col" className="w-[150px] text-base font-semibold text-white">
+                <th scope="col" className="w-[150px] text-sm font-semibold text-white">
                   비고
                 </th>
               </tr>
@@ -30,11 +30,11 @@ export default function Technical3() {
             <tbody className=" text-[#6f6f6f]">
               {researchData.map((el, index) => (
                 <tr className={`text-center bg-[${el.color}]`} key={index}>
-                  <td className="py-4 px-3 text-xl font-normal border-2 border-[#e2e2e2]">{el.complete} </td>
-                  <td className="text-base font-normal border-2 border-[#e2e2e2]">{el.productName}</td>
-                  <td className="text-base font-normal border-2 border-[#e2e2e2]">{el.contents}</td>
-                  <td className="text-base font-normal border-2 border-[#e2e2e2]">{el.deliveryDestination}</td>
-                  <td className="text-base font-normal border-2 border-[#e2e2e2]">{el.etc}</td>
+                  <td className="py-4 px-3 text-sm font-normal border-2 border-[#e2e2e2]">{el.complete} </td>
+                  <td className="text-sm font-normal border-2 border-[#e2e2e2]">{el.productName}</td>
+                  <td className="text-sm font-normal border-2 border-[#e2e2e2]">{el.contents}</td>
+                  <td className="text-sm font-normal border-2 border-[#e2e2e2]">{el.deliveryDestination}</td>
+                  <td className="text-sm font-normal border-2 border-[#e2e2e2]">{el.etc}</td>
                 </tr>
               ))}
             </tbody>
--- src/components/companyInfo/Technical/Technical4.tsx
+++ src/components/companyInfo/Technical/Technical4.tsx
@@ -17,26 +17,32 @@ export default function Technical4() {
       <div className="technical-zoom-container flex flex-col w-full mt-9 items-center">
         <div className="flex flex-row mt-12">
           <div className="flex flex-col text-center items-center mx-4">
-            <Image src="/info/prize/1_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
+            <Image
+              className="max-h-[26.6rem]"
+              src="/info/prize/1_800px.jpg"
+              alt="천마 전지 축전식"
+              width={310}
+              height={430}
+            />
 
-            <p className="text-lg py-2 text-[#909090]">천마 / 전지, 축전식</p>
+            <p className="text-sm py-2 text-[#909090]">천마 / 전지, 축전식</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">육군군수사</p>
+            <p className="text-sm py-2 text-[#909090]">육군군수사</p>
           </div>
 
           <div className="flex flex-col text-center items-center mx-4">
             <Image src="/info/prize/2_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
 
-            <p className="text-lg py-2 text-[#909090]">500MD 헬기 / 17AMP 밧데리</p>
+            <p className="text-sm py-2 text-[#909090]">500MD 헬기 / 17AMP 밧데리</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">육군군수사</p>
+            <p className="text-sm py-2 text-[#909090]">육군군수사</p>
           </div>
 
           <div className="flex flex-col text-center items-center mx-4">
             <Image src="/info/prize/3_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
-            <p className="text-lg py-2 text-[#909090]">500MD 헬기 / 17AMP 밧데리</p>
+            <p className="text-sm py-2 text-[#909090]">500MD 헬기 / 17AMP 밧데리</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">육군군수사</p>
+            <p className="text-sm py-2 text-[#909090]">육군군수사</p>
           </div>
         </div>
 
@@ -44,25 +50,31 @@ export default function Technical4() {
           <div className="flex flex-col text-center items-center mx-4">
             <Image src="/info/prize/4_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
 
-            <p className="text-lg py-2 text-[#909090]">F-4, F-5 항공기 / CELL, BATTERY</p>
+            <p className="text-sm py-2 text-[#909090]">F-4, F-5 항공기 / CELL, BATTERY</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">국방기술품질원</p>
+            <p className="text-sm py-2 text-[#909090]">국방기술품질원</p>
           </div>
 
           <div className="flex flex-col text-center items-center mx-4">
             <Image src="/info/prize/5_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
 
-            <p className="text-lg py-2 text-[#909090]">KT-1, KA-1 / 전지, 축전식</p>
+            <p className="text-sm py-2 text-[#909090]">KT-1, KA-1 / 전지, 축전식</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">국방기술품질원</p>
+            <p className="text-sm py-2 text-[#909090]">국방기술품질원</p>
           </div>
 
           <div className="flex flex-col text-center items-center mx-4">
-            <Image src="/info/prize/6_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
+            <Image
+              className="min-h-[27.5rem]"
+              src="/info/prize/6_800px.jpg"
+              alt="천마 전지 축전식"
+              width={310}
+              height={430}
+            />
 
-            <p className="text-lg py-2 text-[#909090]">UH-1H헬기,AH-1J헬기/전지,축전지</p>
+            <p className="text-sm py-2 text-[#909090]">UH-1H헬기,AH-1J헬기/전지,축전지</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">국방기술품질원</p>
+            <p className="text-sm py-2 text-[#909090]">국방기술품질원</p>
           </div>
         </div>
 
@@ -70,17 +82,17 @@ export default function Technical4() {
           <div className="flex flex-col text-center items-center mx-4">
             <Image src="/info/prize/7_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
 
-            <p className="text-lg py-2 text-[#909090]">UH-60 헬기 / 셀, 리치</p>
+            <p className="text-sm py-2 text-[#909090]">UH-60 헬기 / 셀, 리치</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">육군군수사</p>
+            <p className="text-sm py-2 text-[#909090]">육군군수사</p>
           </div>
 
           <div className="flex flex-col text-center items-center mx-4">
             <Image src="/info/prize/8_800px.jpg" alt="천마 전지 축전식" width={310} height={430} />
 
-            <p className="text-lg py-2 text-[#909090]">견인 발칸 / 전지, 축전식</p>
+            <p className="text-sm py-2 text-[#909090]">견인 발칸 / 전지, 축전식</p>
             <div className="w-32 border-[2.3px] border-green-700"></div>
-            <p className="text-lg py-2 text-[#909090]">육군군수사</p>
+            <p className="text-sm py-2 text-[#909090]">육군군수사</p>
           </div>
         </div>
       </div>
