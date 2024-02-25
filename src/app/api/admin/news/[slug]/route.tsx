6aec7849ecd6734480d4392f7e5c0fbba79bbcb3
김 형석 <neoself1105@gmail.com>
2024-02-26 02:14:44 +0900

    newsBugFix

--- src/app/api/admin/news/[slug]/route.tsx
+++ src/app/api/admin/news/[slug]/route.tsx
@@ -6,8 +6,10 @@ export const GET = async (request: NextRequest, { params }: any) => {
   const { slug } = params
   try {
     connectToDb()
+    console.log('heeloo')
     const newsList = await Post.findOne({ id: 0 })
-    return NextResponse.json(newsList[slug])
+    console.log(newsList)
+    return NextResponse.json(newsList.data[slug])
   } catch (err) {
     console.log(err)
     throw new Error('Failed to fetch dd!')
