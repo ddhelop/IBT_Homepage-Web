8f832ea1fb74eb0ab060127e4f3eae0fa1def6ad
김 형석 <neoself1105@gmail.com>
2024-02-29 00:41:55 +0900

    bugFix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { securedURLs } from './lib/data'
 
+const securedURLs = [
+  `${process.env.URL}/admin/news`,
+  `${process.env.URL}/admin/catelogs`,
+  `${process.env.URL}/admin/batteries`,
+  `${process.env.URL}/admin/esg-pdf`,
+]
 export function middleware(request: NextRequest) {
   if (!request.cookies.has('session') && securedURLs.indexOf(request.url) !== -1) {
+    //admin/으로 시작하는 url에 접근하는 중일때
     console.log('쿠키가 없습니다.')
     return NextResponse.redirect(new URL('/admin', request.url))
   }
+  console.log(securedURLs.indexOf(request.url))
   return NextResponse.next()
 }
