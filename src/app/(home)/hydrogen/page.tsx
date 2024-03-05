be4ad2f0d5b7925ca97c00728ad080e966c36097
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-03-05 14:27:11 +0900

    feat: :sparkles: hydrogen  임시

--- src/app/(home)/hydrogen/page.tsx
+++ src/app/(home)/hydrogen/page.tsx
@@ -1,5 +1,6 @@
 import HydrogenPage from '@/components/hydrogen/HydrogenPage'
 import { Metadata } from 'next'
+import Image from 'next/image'
 
 export const metadata = {
   title: '(주)아이비티 수소',
@@ -10,8 +11,14 @@ export default function pagHydrogenPage() {
   return (
     // <div className="flex justify-center min-h-[90vh]">
     // <div className="w-[min(92%,1080px)]">
-    <HydrogenPage />
+    // <HydrogenPage />
     // </div>
     // </div>
+
+    // 임시
+    <div className="flex flex-col w-full items-center justify-center">
+      <p className="text-4xl text-[#79AD4B] mt-48 mb-32">HYDROGEN</p>
+      <Image alt="hydrogen" src={'/image/hydrogen/hydrogen_intro.png'} width={1920} height={1183} />
+    </div>
   )
 }
