5b088daef2bf8e69212abb0aa1121047c7b1acde
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-03-16 01:54:27 +0900

    feat: hydrogen 영문 이미지 추가

--- src/app/(home)/hydrogen/page.tsx
+++ src/app/(home)/hydrogen/page.tsx
@@ -1,16 +1,13 @@
-import Image from 'next/image'
+import TempHydrogenComponent from '@/components/hydrogen/TempHydrogenComponent'
 
 export const metadata = {
   title: '(주)아이비티 수소',
   description: 'IBT 수소 제품 소개 페이지',
 }
 
-export default function pagHydrogenPage() {
+export default function PagHydrogenPage() {
   return (
     // 임시
-    <div className="flex flex-col w-full items-center justify-center">
-      <p className="text-4xl text-[#79AD4B] mt-48 mb-32">HYDROGEN</p>
-      <Image alt="hydrogen" src={'/image/hydrogen/hydrogen_intro.png'} width={1920} height={1183} />
-    </div>
+    <TempHydrogenComponent />
   )
 }
