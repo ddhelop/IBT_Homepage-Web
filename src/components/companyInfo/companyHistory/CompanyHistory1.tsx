00fd841a640620d19a42cde0062a7b40a854c499
김 형석 <neoself1105@gmail.com>
2024-03-15 18:51:20 +0900

    designFix

--- src/components/companyInfo/companyHistory/CompanyHistory1.tsx
+++ src/components/companyInfo/companyHistory/CompanyHistory1.tsx
@@ -17,10 +17,12 @@ export default function CompanyHistory1() {
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-5">2023</h3>
               <p className="font-light text-xl lg:text-sm break-keep mb-16">{CompanyInfoData[2][2023]?.[isEnglish]}</p>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-5">2023</h3>
+              <p className="font-light text-xl lg:text-sm break-keep mb-16">{CompanyInfoData[2][2023]?.[isEnglish]}</p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
-              <h3 className="text-4xl lg:text-xl  font-bold tracking-normal text-[#59A833] mb-5">2021</h3>
+              <h3 className="text-4xl lg:text-xl  lg:text-xl font-bold tracking-normal text-[#59A833] mb-5">2021</h3>
               <p className="font-light text-xl lg:text-sm break-keep ">{CompanyInfoData[2][2021]?.[isEnglish]}</p>
 
               <Image
@@ -41,16 +43,22 @@ export default function CompanyHistory1() {
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-3">2020</h3>
               <p className="font-light text-xl lg:text-sm break-keep  mb-7">{CompanyInfoData[2][2020]?.[isEnglish]}</p>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-3">2020</h3>
+              <p className="font-light text-xl lg:text-sm break-keep  mb-7">{CompanyInfoData[2][2020]?.[isEnglish]}</p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-4">2019</h3>
-              <p className="font-light text-xl lg:text-sm break-keep mb-7">{CompanyInfoData[2][2019]?.[isEnglish]}</p>
+              <p className="font-light text-xl lg:text-sm break-keep mb-7 leading-6">
+                {CompanyInfoData[2][2019]?.[isEnglish]}
+              </p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-4">2018</h3>
-              <p className="font-light text-xl lg:text-sm break-keep mb-7">{CompanyInfoData[2][2018]?.[isEnglish]}</p>
+              <p className="font-light text-xl lg:text-sm break-keep mb-7 leading-6">
+                {CompanyInfoData[2][2018]?.[isEnglish]}
+              </p>
               <Image
                 src="/info/2018.png" // 이미지 경로
                 alt="2018"
@@ -63,10 +71,14 @@ export default function CompanyHistory1() {
 
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-4">2017</h3>
-              <p className="font-light text-xl lg:text-sm break-keep mb-7">{CompanyInfoData[2][2017]?.[isEnglish]}</p>
+              <p className="font-light text-xl lg:text-sm break-keep leading-6 mb-7">
+                {CompanyInfoData[2][2017]?.[isEnglish]}
+              </p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-4">2015</h3>
+              <p className="font-light text-xl lg:text-sm break-keep mb-7 ">{CompanyInfoData[2][2015]?.[isEnglish]}</p>
               <h3 className="text-4xl lg:text-xl font-bold tracking-normal text-[#59A833] mb-4">2015</h3>
               <p className="font-light text-xl lg:text-sm break-keep mb-7 ">{CompanyInfoData[2][2015]?.[isEnglish]}</p>
             </LeftMotionComponent>
--- src/components/companyInfo/companyHistory/CompanyHistory3.tsx
+++ src/components/companyInfo/companyHistory/CompanyHistory3.tsx
@@ -17,14 +17,20 @@ export default function CompanyHistory3() {
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-3">2005</h3>
               <p className="font-light text-xl lg:text-sm mb-10 break-keep">{CompanyInfoData[2][2005]?.[isEnglish]}</p>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-3">2005</h3>
+              <p className="font-light text-xl lg:text-sm mb-10 break-keep">{CompanyInfoData[2][2005]?.[isEnglish]}</p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-3">2004</h3>
-              <p className="font-light text-xl lg:text-sm mb-10 break-keep">{CompanyInfoData[2][2004]?.[isEnglish]}</p>
+              <p className="font-light text-xl lg:text-sm leading-7 mb-10 break-keep">
+                {CompanyInfoData[2][2004]?.[isEnglish]}
+              </p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-3">2003</h3>
+              <p className="font-light text-xl lg:text-sm mb-8 break-keep">{CompanyInfoData[2][2004]?.[isEnglish]}</p>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-3">2003</h3>
               <p className="font-light text-xl lg:text-sm mb-8 break-keep">{CompanyInfoData[2][2004]?.[isEnglish]}</p>
             </LeftMotionComponent>
@@ -46,14 +52,20 @@ export default function CompanyHistory3() {
 
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-3">2002</h3>
               <p className="font-light text-xl lg:text-sm break-keep mb-10">{CompanyInfoData[2][2002]?.[isEnglish]}</p>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-3">2002</h3>
+              <p className="font-light text-xl lg:text-sm break-keep mb-10">{CompanyInfoData[2][2002]?.[isEnglish]}</p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">2001</h3>
               <p className="font-light text-xl lg:text-sm break-keep mb-6">{CompanyInfoData[2][2001]?.[isEnglish]}</p>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">2001</h3>
+              <p className="font-light text-xl lg:text-sm break-keep mb-6">{CompanyInfoData[2][2001]?.[isEnglish]}</p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">2000</h3>
+              <p className="font-light text-xl lg:text-sm break-keep mb-10">{CompanyInfoData[2][2000]?.[isEnglish]}</p>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">2000</h3>
               <p className="font-light text-xl lg:text-sm break-keep mb-10">{CompanyInfoData[2][2000]?.[isEnglish]}</p>
 
@@ -70,9 +82,13 @@ export default function CompanyHistory3() {
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1998</h3>
               <p className="font-light text-xl lg:text-sm mb-7 break-keep">{CompanyInfoData[2][1998]?.[isEnglish]}</p>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1998</h3>
+              <p className="font-light text-xl lg:text-sm mb-7 break-keep">{CompanyInfoData[2][1998]?.[isEnglish]}</p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1993</h3>
+              <p className="font-light text-xl lg:text-sm mb-7 break-keep">{CompanyInfoData[2][1993]?.[isEnglish]}</p>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1993</h3>
               <p className="font-light text-xl lg:text-sm mb-7 break-keep">{CompanyInfoData[2][1993]?.[isEnglish]}</p>
             </LeftMotionComponent>
@@ -80,9 +96,13 @@ export default function CompanyHistory3() {
             <LeftMotionComponent component={motion.div}>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1987</h3>
               <p className="font-light text-xl lg:text-sm mb-7 break-keep">{CompanyInfoData[2][1987]?.[isEnglish]}</p>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1987</h3>
+              <p className="font-light text-xl lg:text-sm mb-7 break-keep">{CompanyInfoData[2][1987]?.[isEnglish]}</p>
             </LeftMotionComponent>
 
             <LeftMotionComponent component={motion.div}>
+              <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1986</h3>
+              <p className="font-light text-xl lg:text-sm mb-7 break-keep ">{CompanyInfoData[2][1986]?.[isEnglish]}</p>
               <h3 className="text-4xl lg:text-xl font-bold tracking-[-0.38px] text-[#59A833] mb-4">1986</h3>
               <p className="font-light text-xl lg:text-sm mb-7 break-keep ">{CompanyInfoData[2][1986]?.[isEnglish]}</p>
             </LeftMotionComponent>
@@ -90,6 +110,7 @@ export default function CompanyHistory3() {
 
           <div className="hidden lg:flex absolute lg:left-[-2%] xl:left-[6%] 2xl:left-[10%]">
             <RightMotionComponent component={motion.div}>
+              <p className="text-[#2B6434] font-bold tracking-[1.059px] lg:text-xl ">
               <p className="text-[#2B6434] font-bold tracking-[1.059px] lg:text-xl ">
                 {CompanyInfoData[2].text3?.[isEnglish]}
               </p>
