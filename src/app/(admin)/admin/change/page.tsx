fb01197afc574bac7277fa164418f05717ae4cbd
김 형석 <neoself1105@gmail.com>
2024-03-24 12:48:35 +0900

    bugfix

--- src/app/(admin)/admin/change/page.tsx
+++ src/app/(admin)/admin/change/page.tsx
@@ -3,7 +3,7 @@ import ChangeAccount from '@/components/admin/ChangeAccount'
 const ChangePage = async () => {
   return (
     <div className="flex flex-col flex-1 h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4 bg-white p-8">관리자 계정 변경</h1>
+      <h1 className="text-2xl font-bold mb-4 bg-white p-8">관리자 비밀번호 변경</h1>
       <ChangeAccount />
     </div>
   )
